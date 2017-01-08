048c01de57dc62d98eaf62aa5eef01af843aeddd
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2017-01-08 19:31:46 +0100

    a little clean up of the unused files and test cases

--- the-movie-db-grabber/src/main/java/com/thecatalog/grabber/DataParser.java
+++
@@ -1,7 +0,0 @@
-package com.thecatalog.grabber;
-
-@FunctionalInterface
-public interface DataParser<T> {
-
-    T parse(byte[] bytes);
-}
--- the-movie-db-grabber/src/main/java/com/thecatalog/grabber/util/ToBooleanFunction.java
+++
@@ -1,6 +0,0 @@
-package com.thecatalog.grabber.util;
-
-@FunctionalInterface
-public interface ToBooleanFunction<T> {
-    boolean apply(T value);
-}
